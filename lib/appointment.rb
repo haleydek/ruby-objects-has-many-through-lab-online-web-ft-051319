d7ebbe31514cc0daf1c629146f71aac2c49b515d
Haley DeKeyser <hcd09@live.com>
2019-05-29 20:11:36 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -3,7 +3,7 @@ class Appointment
   attr_reader :date
   @@all = []
   
-  def initialize(date, patient, doctor)
+  def initialize(patient, doctor)
     @date = date
     @patient = patient
     @doctor = doctor
